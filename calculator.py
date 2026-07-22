50c70fa05ea480fa500e36d23d4619da3b356d39
MengerField <racamansequence@proton.me>
2026-07-22 10:51:15 -0600

    Add power raising functionality to calculator
    thats what she said

--- calculator.py
+++ calculator.py
@@ -4,3 +4,12 @@ num2=float(input("second digit: "))
 sum = num1+num2
 
 print(sum)
+
+print("Raise to power")
+def raise_to_power(base_num, pow_num):
+        result=1
+        for index in range(pow_num):
+                result = result * base_num
+        return result
+
+print(raise_to_power(int(input("1num: ")),int(input("2num: "))))
