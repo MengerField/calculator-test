b1361ebdafceab468e58e9fdedd4166c211f2528
MengerField <racamansequence@proton.me>
2026-07-21 19:50:53 -0600

    Change input to float and add print statement
    yeah, what he say

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
-num1=int(input("first digit: "))
-num2=int(input("second digit: "))
+print("Addition of two numbers")
+num1=float(input("first digit: "))
+num2=float(input("second digit: "))
 sum = num1+num2
 
 print(sum)
