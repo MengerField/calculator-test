050f86caaaaee4a88028c6255d3d2ecc9b4817aa
MengerField <racamansequence@proton.me>
2026-07-23 14:01:15 -0600

    Refactor calculator to include options for addition and power
    to the merry-go-round and round

--- calculator.py
+++ calculator.py
@@ -1,15 +1,20 @@
-print("Addition of two numbers")
-num1=float(input("first digit: "))
-num2=float(input("second digit: "))
-sum = num1+num2
+print("Option A to sum 2 numbers")
+print("Option B to raise to power")
 
-print(sum)
+option=(input("Choose an option: "))
+if option.lower()=="a":
+        print("Addition of two numbers")
+        num1=float(input("first digit: "))
+        num2=float(input("second digit: "))
+        sum = num1+num2
+        print(sum)
+else:
+        print("Raise to power")
+        def raise_to_power(base_num, pow_num):
+                result=1
+                for index in range(pow_num):
+                        result = result * base_num
+                return result
 
-print("Raise to power")
-def raise_to_power(base_num, pow_num):
-        result=1
-        for index in range(pow_num):
-                result = result * base_num
-        return result
+        print(raise_to_power(int(input("1num: ")),int(input("2num: "))))
 
-print(raise_to_power(int(input("1num: ")),int(input("2num: "))))
