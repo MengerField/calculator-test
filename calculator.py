f0b6ed5012e83567702def81901db025be87684c
MengerField <racamansequence@proton.me>
2026-07-24 10:18:57 -0600

    Fix typos in calculator.py
    why waste time say lot word when few words do trick?

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
-#GNU nano 7.2                      calculator.py                               
+#   GNU nano 7.2                      calculator.py  
 print("Option A to addition of 2 numbers")
 print("Option B to raise to power")
 
 option=(input("Choose an option: "))
 if option.lower()=="a":
-        print("Input 2 humbers")
+        print("Input 2 numbers")
         num1=float(input("first digit: "))
         num2=float(input("second digit: "))
-        print("Option 1 to addition")
-        print("Option 2 to substraction")
+        print("Option A to addition")
+        print("Option B to subtraction")
         option2=(input("Choose an option: "))
         if option2.lower()=="a":
                 sum = num1+num2
@@ -19,4 +19,3 @@ if option.lower()=="a":
 else:
         print("Raise to power")
         def raise_to_power(base_num, pow_num):
-
