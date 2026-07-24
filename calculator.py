00394c059dbab291846f49f7a13670364a77d3bb
MengerField <racamansequence@proton.me>
2026-07-24 10:13:09 -0600

    add a subtraction option inside the adition
    Major calculator upgrade: 
    introduced an operation selection menu with support for both addition and subtraction. the project has now reached the impressive milestone of handling three whole arithmetic operations. 
    
    Dont overwhelm with it

--- calculator.py
+++ calculator.py
@@ -1,20 +1,22 @@
-print("Option A to sum 2 numbers")
+#GNU nano 7.2                      calculator.py                               
+print("Option A to addition of 2 numbers")
 print("Option B to raise to power")
 
 option=(input("Choose an option: "))
 if option.lower()=="a":
-        print("Addition of two numbers")
+        print("Input 2 humbers")
         num1=float(input("first digit: "))
         num2=float(input("second digit: "))
-        sum = num1+num2
-        print(sum)
+        print("Option 1 to addition")
+        print("Option 2 to substraction")
+        option2=(input("Choose an option: "))
+        if option2.lower()=="a":
+                sum = num1+num2
+                print(sum)
+        else:
+                res = num1-num2
+                print(res)
 else:
         print("Raise to power")
         def raise_to_power(base_num, pow_num):
-                result=1
-                for index in range(pow_num):
-                        result = result * base_num
-                return result
-
-        print(raise_to_power(int(input("1num: ")),int(input("2num: "))))
 
